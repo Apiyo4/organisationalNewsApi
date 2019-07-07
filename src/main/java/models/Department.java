b9bee5b4bd5d394235805f743d9c9fc9cc804c39
Annette Odingo <odingoannette@gmail.com>
2019-07-08 02:43:36 +0300

    Add test to see if Department instantiates correctly, and for it's getters and setters and if it returns true when information is same

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,14 +1,55 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
     private int id;
     private String departmentName;
     private String description;
-    private int numberOfEmoployees;
+    private int numberOfEmployees;
 
     public Department(String departmentName, String description, int numberOfEmployees) {
         this.departmentName = departmentName;
         this.description = description;
-        this.numberOfEmoployees = numberOfEmoployees;
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return numberOfEmployees == that.numberOfEmployees &&
+                Objects.equals(departmentName, that.departmentName) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentName, description, numberOfEmployees);
     }
 }

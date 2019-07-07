2d686ccefb893b2d42c7d6b9816a004fad5fd669
Annette Odingo <odingoannette@gmail.com>
2019-07-08 02:21:19 +0300

    Generate a test file with tearDown and setUp annotations for Department.java

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,14 @@
 package models;
 
 public class Department {
+    private int id;
+    private String departmentName;
+    private String description;
+    private int numberOfEmoployees;
+
+    public Department(String departmentName, String description, int numberOfEmployees) {
+        this.departmentName = departmentName;
+        this.description = description;
+        this.numberOfEmoployees = numberOfEmoployees;
+    }
 }

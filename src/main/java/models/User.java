50fb5b695851e398a74a9c62e30373de77aa7915
Annette Odingo <odingoannette@gmail.com>
2019-07-08 02:45:39 +0300

    Generate a test file with tearDown and setUp annotations for User.java

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,14 @@
 package models;
 
 public class User {
+    private int id;
+    private String userName;
+    private String departmentsId;
+    private String role;
+
+    public User(String userName, String departmentsId, String role) {
+        this.userName = userName;
+        this.departmentsId = departmentsId;
+        this.role = role;
+    }
 }

f0a623a2c97a91e8c1124f980707a08f2f51f604
Annette Odingo <odingoannette@gmail.com>
2019-07-08 04:15:52 +0300

    Add content in UserDao

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.User;
+import models.Department;
+
+import java.util.List;
+
 public interface UserDao {
+    //create
+    void add(User user);
+    //void addUserToDepartment(User user, Department department);
+
+    //read
+    List<User> getAll();
+   // List<Department> getAllDepartmentsForAUser(int id);
+    User findById(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }

0029ca3c3649b6b368e8fc1de00e7993d2a1cf9d
Annette Odingo <odingoannette@gmail.com>
2019-07-08 03:04:02 +0300

    Add test to see if User instantiates correctly, and for it's getters and setters and if it returns true when information is same

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,14 +1,55 @@
 package models;
 
+import java.util.Objects;
+
 public class User {
     private int id;
     private String userName;
-    private String departmentsId;
+    private int departmentId;
     private String role;
 
-    public User(String userName, String departmentsId, String role) {
+    public User(String userName, int departmentId, String role) {
         this.userName = userName;
-        this.departmentsId = departmentsId;
+        this.departmentId = departmentId;
         this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return departmentId == user.departmentId &&
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, departmentId, role);
     }
 }

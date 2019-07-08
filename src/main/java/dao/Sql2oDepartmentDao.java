d1fe88edc3331c6a69b4354a8686bb8453af3f50
Annette Odingo <odingoannette@gmail.com>
2019-07-08 07:17:42 +0300

    Create Sql2oDepartmentDaoTest.java

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -1,4 +1,17 @@
 package dao;
 
-public class Sql2oDepartmentDao {
+import models.User;
+import models.Department;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+
+public class Sql2oDepartmentDao{
+    private final Sql2o sql2o;
+    public Sql2oDepartmentDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+
 }

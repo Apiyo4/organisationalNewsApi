38b075a54f1057392f50ba5a26f873c140eef04e
Annette Odingo <odingoannette@gmail.com>
2019-07-09 15:21:37 +0300

    Add path for department routes

---
+++ src/main/java/App.java
@@ -0,0 +1,48 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUserDao;
+import models.Department;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class App {
+
+    public static void main(String[] args) {
+        Sql2oUserDao userDao;
+        Sql2oDepartmentDao departmentDao;
+        Sql2oNewsDao newsDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/organisational-news-api.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        conn = sql2o.open();
+
+        post("/departments/new", "application/json", (req, res) -> {
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.add(department);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(department);
+        });
+
+        get("/departments", "application/json", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(departmentDao.getAll());
+        });
+
+        get("/departments/:id", "application/json", (req, res) -> {
+            res.type("application/json");
+            int departmentId = Integer.parseInt(req.params("id"));
+            res.type("application/json");
+            return gson.toJson(departmentDao.findById(departmentId));
+        });
+    }
+}

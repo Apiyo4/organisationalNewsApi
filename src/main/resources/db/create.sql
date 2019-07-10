386fb22c9164f8aeaf6c46db7fff616a6c5dc2fe
Annette Odingo <odingoannette@gmail.com>
2019-07-10 06:52:09 +0300

    Create database in create.sql and create drop.sql

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,4 +1,5 @@
-SET MODE PostgreSQL;
+CREATE DATABASE organisational_news_api;
+\c organisational_news_api;
 
 CREATE TABLE IF NOT EXISTS departments (
  id int PRIMARY KEY auto_increment,
@@ -13,9 +14,12 @@ CREATE TABLE IF NOT EXISTS users (
  departmentId int,
  role VARCHAR
 );
+
 CREATE TABLE IF NOT EXISTS news (
  id int PRIMARY KEY auto_increment,
  content varchar,
  departmentId int,
  description VARCHAR
  );
+
+CREATE DATABASE organisational_news_api_test WITH TEMPLATE organisational_news_api;
